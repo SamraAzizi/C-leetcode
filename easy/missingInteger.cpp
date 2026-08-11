a8938fdd65051cd22cdaf2f28361ff043d1758d0
SamraAzizi <samarraazizi@gmail.com>
2026-08-11 14:51:53 +0500

    easy c++ leetcode

---
+++ easy/missingInteger.cpp
@@ -0,0 +1,15 @@
+class Solution{
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, 0);
+        dp[0] = nums[0];
+        int maxSum = dp[0];
+
+        for(int i=1; i<n; i++){
+            dp[i] = max(nums[i], nums[i]+dp[i-1]);
+            maxSum = max(maxSum, dp[i]);
+        }
+        return maxSum;
+    }
+}
